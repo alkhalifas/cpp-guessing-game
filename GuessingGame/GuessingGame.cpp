c2cb048b4277199ece28d2023b3fa4bef1f76ebf
Saleh Alkhalifa <salehesam@gmail.com>
2021-03-14 22:29:44 -0400

    Program completed and working

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -1,33 +1,66 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib>
+#include <ctime>
 
-int main()
-{
-	int secretNum = 7;
-	int guess = 0;
-	int guessCount = 0;
-	int guessLimit = 3;
-	bool outOfGuesses = false;
+int main(void) {
+	srand(time(NULL));
 
-	while (secretNum != guess && !outOfGuesses) {
-		if (guessCount < guessLimit) {
-			cout << "Enter a Guess: ";
-			cin >> guess;
-			guessCount++;
+	while (true) {
+		int number = rand() % 99 + 2;
+		int guess;
+		int tries = 0;
+		char answer;
+
+		while (true) {
+			std::cout << "Guess a number between 1 and 100 (" << 20 - tries << " tries left): ";
+			std::cin >> guess;
+			std::cin.ignore();
+
+			if (tries >= 20) {
+				break;
+			}
+
+			if (guess > number) {
+				std::cout << "Too High! Try again! \n";
+			}
+			else if (guess < number) {
+				std::cout << "Too Low! Try again! \n";
+			}
+			else {
+				break;
+			}
+			tries++;
+		}
+
+		if (tries >= 20) {
+			std::cout << "You ran out of tries! \n\n";
 		}
 		else {
-			outOfGuesses = true;
+			std::cout << "Congratulations! " << std::endl;
+			std::cout << "You got it right in " << tries << " tries.\n";
 		}
+		while(true) {
+			std::cout << "Play again? (Y/N)? ";
+			std::cin >> answer;
+			std::cin.ignore();
 
+			if (answer == 'n' || answer == 'N' || answer == 'y' || answer == 'Y') {
+				break;
+			}
+			else {
+				std::cout << "Please Enter \'Y\' or \'N\' ... \n";
+			}
+		}
+		if (answer == 'n' || answer == 'N') {
+			std::cout << "Thank you for playing!";
+			break;
+		}
+		else {
+			std::cout << "\n\n\n";
+		}
 	}
 
-	if (outOfGuesses) {
-		cout << "You Lose!";
-	}
-	else {
-		cout << "You Win!";
-	}
-
-
+	std::cout << "\n\nEnter anything to exit...";
+	std::cin.ignore();
 	return 0;
 }
